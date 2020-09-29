c41c96e0e91ddfdf76a1deafc17173b68eafedcf
DemonKing252 <46793138+DemonKing252@users.noreply.github.com>
2020-09-29 15:11:43 -0400

    Fixed landscape left/right.

---
+++ Assets/Scrips/CameraScripts/CameraScript_Instructions.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/***************
+File: CameraScript_Instructions.cs
+Author: Liam Blake
+Created: 2020-09-29
+Modified: 2020-09-29
+Desc:
+    Manager for the camera for this scene.
+***************/
+public class CameraScript_Instructions : MonoBehaviour
+{
+    public float ResolutionScaleX = 1.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Screen.orientation == ScreenOrientation.LandscapeLeft ||
+            Screen.orientation == ScreenOrientation.LandscapeRight)
+        {
+            Screen.SetResolution((int)(1440 * ResolutionScaleX), 2560, true);
+        }
+        else
+        {
+            Screen.SetResolution(1440, 2560, true);
+        }
+        
+    }
+
+}
+
---
+++ Assets/Scrips/CameraScripts/CameraScript_Menu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*************************************************
+File: CameraScript_Menu.cs
+Author: Liam Blake
+Created: 2020-09-29
+Modified: 2020-09-29
+Desc:
+    Manager for the camera for this scene.
+*************************************************/
+public class CameraScript_Menu : MonoBehaviour
+{
+    public float ResolutionScaleX = 1.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Screen.orientation == ScreenOrientation.LandscapeLeft ||
+            Screen.orientation == ScreenOrientation.LandscapeRight)
+        {
+            Screen.SetResolution((int)(1440* ResolutionScaleX), 2560, true);
+        }
+        else
+        {
+            Screen.SetResolution(1440, 2560, true);
+        }
+    }
+}

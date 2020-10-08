bd5a6d92c9f3fa0a2a409605d9fa7c3b05dc8f1d
DemonKing252 <46793138+DemonKing252@users.noreply.github.com>
2020-10-08 02:10:37 -0400

    Player can shoot and now rotates to the position of a touch event.

---
+++ Assets/Scrips/BulletController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    [SerializeField]
+    float speed;
+
+    private float angle;
+    private Vector3 rotation;
+    // Start is called before the first frame update
+
+    private Vector2 delta;
+    [System.Obsolete]
+    void Start()
+    {
+        rotation = gameObject.transform.rotation.ToEulerAngles();
+        angle = rotation.z + Mathf.PI / 2.0f;
+
+        gameObject.transform.rotation = Quaternion.EulerAngles(0.0f, 0.0f, angle + Mathf.PI / 2.0f);
+
+        delta = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
+
+        Debug.Log("Bullet created!");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(rotation.ToString());
+        gameObject.transform.position += new Vector3(delta.x, delta.y, 0.0f) * (speed * Time.deltaTime);
+
+        CheckBounds();
+    }
+    private void CheckBounds()
+    {
+        
+        bool destroy = false;
+        if (gameObject.transform.position.y >= Camera.main.ScreenToWorldPoint(new Vector3(0.0f, Screen.safeArea.height, 0.0f)).y)
+            destroy = true;
+        else if (gameObject.transform.position.y <= -Camera.main.ScreenToWorldPoint(new Vector3(0.0f, Screen.safeArea.height, 0.0f)).y)
+            destroy = true;
+        else if (gameObject.transform.position.x <= -Camera.main.ScreenToWorldPoint(new Vector3(Screen.safeArea.width, 0.0f, 0.0f)).x)
+            destroy = true;
+        else if (gameObject.transform.position.x >= Camera.main.ScreenToWorldPoint(new Vector3(Screen.safeArea.width, 0.0f, 0.0f)).x)
+            destroy = true;
+
+        if (destroy)
+            Destroy(gameObject);
+    }
+}
---
+++ Assets/Scrips/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    GameObject bulletPrefab;
+
+    [SerializeField]
+    Transform playerCentreLocation;
+
+    [SerializeField]
+    float delayBetweenFiring;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    float time = 0.0f;
+
+    // Update is called once per frame
+    [System.Obsolete]
+    void Update()
+    {
+        time += Time.deltaTime;
+        foreach (var touch in Input.touches)
+        {
+            var touchWorldSpace = Camera.main.ScreenToWorldPoint(touch.position);
+
+            Quaternion rot = Quaternion.identity;
+
+            // These functions have lots of issues
+            /***
+                Vector3 rot2 = Vector3.RotateTowards(gameObject.transform.position, touchWorldSpace, 50.0f, 50.0f);
+                float angle = Vector3.AngleBetween(gameObject.transform.position, touchWorldSpace);
+            ***/
+            Vector2 delta = touchWorldSpace - gameObject.transform.position;
+
+            float angle = Mathf.Atan2(delta.y, delta.x) - (Mathf.PI / 2.0f);
+
+            rot.SetEulerRotation(0.0f, 0.0f, angle);
+
+            gameObject.transform.rotation = rot;
+
+            if (time >= delayBetweenFiring)
+            {
+                time = 0.0f;
+                Instantiate(bulletPrefab, playerCentreLocation.position, gameObject.transform.rotation);
+            }
+        }
+        
+    }
+}
